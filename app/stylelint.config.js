b89569bc62377855236c4c135f7ac0ab3c6b1ff3
Dwight Gunning <dwight@dwightgunning.com>
2018-06-05 13:35:59 +0200

    Linting via eslint for api and app packages

--- app/stylelint.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	extends: 'stylelint-config-standard',
-	plugins: ['stylelint-order'],
-	rules: {
-		'declaration-colon-newline-after': null,
-		indentation: 'tab',
-		'order/properties-alphabetical-order': true,
-	},
-};
